65e6ebeea33dd1edcc94132a6b15f95fe08c07bb
znuznu <znu_1@protonmail.com>
2022-08-23 20:37:32 +0200

    feat: add VoteCount + transaction to the vote commit

--- src/server/router/voteRouter.ts
+++ src/server/router/voteRouter.ts
@@ -8,13 +8,63 @@ export const votingRouter = createRouter().mutation('commit', {
     store: z.object({ pokemonId: z.string() })
   }),
   resolve: async ({ input, ctx }) => {
-    const vote = await ctx.prisma.vote.create({
-      data: {
-        storeId: input.store.pokemonId,
-        keepId: input.keep.pokemonId,
-        releaseId: input.release.pokemonId
+    const storeUpsert = ctx.prisma.voteCount.upsert({
+      where: {
+        pokemonId: input.store.pokemonId
+      },
+      update: {
+        storeCount: {
+          increment: 1
+        }
+      },
+      create: {
+        storeCount: 1,
+        pokemonId: input.store.pokemonId
       }
     });
+
+    const keepUpsert = ctx.prisma.voteCount.upsert({
+      where: {
+        pokemonId: input.keep.pokemonId
+      },
+      update: {
+        keepCount: {
+          increment: 1
+        }
+      },
+      create: {
+        keepCount: 1,
+        pokemonId: input.keep.pokemonId
+      }
+    });
+
+    const releaseUpsert = ctx.prisma.voteCount.upsert({
+      where: {
+        pokemonId: input.release.pokemonId
+      },
+      update: {
+        releaseCount: {
+          increment: 1
+        }
+      },
+      create: {
+        releaseCount: 1,
+        pokemonId: input.release.pokemonId
+      }
+    });
+
+    const [vote] = await ctx.prisma.$transaction([
+      ctx.prisma.vote.create({
+        data: {
+          storeId: input.store.pokemonId,
+          keepId: input.keep.pokemonId,
+          releaseId: input.release.pokemonId
+        }
+      }),
+      storeUpsert,
+      keepUpsert,
+      releaseUpsert
+    ]);
 
     return vote;
   }
