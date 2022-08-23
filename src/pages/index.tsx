c822f403753e23996076b292c1758452b4ed0fa6
znuznu <znu_1@protonmail.com>
2022-08-23 08:21:15 +0200

    feat: add total votes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
+import { trpc } from '@/utils/trpc';
 import type { NextPage } from 'next';
 import React from 'react';
 
 import styles from './Index.module.scss';
 
 const Home: NextPage = () => {
+  const { data: totalCount } = trpc.useQuery(['vote.getTotal']);
+
   return (
     <div className={styles.home}>
-      <p>
+      <p className={styles.explanation}>
         This website is based on the popular <em>Kiss, Marry, Kill game</em>, but you must
         choose between keeping a Pokemon in your team, storing one in your PC and
         releasing one into the wild.
       </p>
+      <h1>Hall of fame</h1>
+      {totalCount && <p>Total votes: {totalCount} </p>}
     </div>
   );
 };
--- src/server/router/voteRouter.ts
+++ src/server/router/voteRouter.ts
@@ -1,21 +1,27 @@
 import { z } from 'zod';
 import { createRouter } from './context';
 
-export const votingRouter = createRouter().mutation('commit', {
-  input: z.object({
-    keep: z.object({ pokemonId: z.string() }),
-    release: z.object({ pokemonId: z.string() }),
-    store: z.object({ pokemonId: z.string() })
-  }),
-  async resolve({ input, ctx }) {
-    const vote = await ctx.prisma.vote.create({
-      data: {
-        storeId: input.store.pokemonId,
-        keepId: input.keep.pokemonId,
-        releaseId: input.release.pokemonId
-      }
-    });
+export const votingRouter = createRouter()
+  .mutation('commit', {
+    input: z.object({
+      keep: z.object({ pokemonId: z.string() }),
+      release: z.object({ pokemonId: z.string() }),
+      store: z.object({ pokemonId: z.string() })
+    }),
+    resolve: async ({ input, ctx }) => {
+      const vote = await ctx.prisma.vote.create({
+        data: {
+          storeId: input.store.pokemonId,
+          keepId: input.keep.pokemonId,
+          releaseId: input.release.pokemonId
+        }
+      });
 
-    return vote;
-  }
-});
+      return vote;
+    }
+  })
+  .query('getTotal', {
+    resolve: async ({ ctx }) => {
+      return ctx.prisma.vote.count();
+    }
+  });
