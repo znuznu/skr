1b54a6003de71dabc922ddbdc5dc36a1babe7cb8
znuznu <znu_1@protonmail.com>
2022-08-28 15:59:47 +0200

    feat: enhance home UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,13 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.home}>
+      <h1>Store, Keep, Release</h1>
       <p className={styles.explanation}>
         This website is based on the popular <em>Kiss, Marry, Kill game</em>, but you must
         choose between keeping a Pokemon in your team, storing one in your PC and
         releasing one into the wild.
       </p>
-      <h1>Hall of fame</h1>
+      <h2>Hall of fame</h2>
       <p>Total votes: {totalCount} </p>
     </div>
   );
