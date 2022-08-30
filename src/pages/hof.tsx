78d9bd87d6b5e28b9e91a8675da3a473a7162d7e
znuznu <znu_1@protonmail.com>
2022-08-30 23:04:46 +0200

    feat(hof): inefficient most voted

--- src/pages/hof.tsx
+++ src/pages/hof.tsx
@@ -2,10 +2,7 @@ import { appRouter } from '@/server/router';
 import { createSSGHelpers } from '@trpc/react/ssg';
 import { createContext } from '@/server/router/context';
 import type { InferGetStaticPropsType } from 'next';
-import Image from 'next/image';
 import React from 'react';
-
-import * as Table from '@/components/primitives/Table';
 
 import styles from './Hof.module.scss';
 import MostVoted from '@/components/MostVoted';
@@ -36,9 +33,9 @@ export const getStaticProps = async () => {
   return {
     props: {
       results: {
-        store: await ssg.fetchQuery('results.mostVoted', { type: 'store' }),
-        keep: await ssg.fetchQuery('results.mostVoted', { type: 'keep' }),
-        release: await ssg.fetchQuery('results.mostVoted', { type: 'release' })
+        store: await ssg.fetchQuery('results.mostStored'),
+        keep: await ssg.fetchQuery('results.mostKeeped'),
+        release: await ssg.fetchQuery('results.mostReleased')
       }
     },
     revalidate: REVALIDATE_TIME_IN_SECONDS
--- src/server/router/resultsRouter.ts
+++ src/server/router/resultsRouter.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { z } from 'zod';
 import { createRouter } from './context';
 
@@ -35,25 +36,176 @@ export const resultsRouter = createRouter()
       }));
     }
   })
-  .query('mostVoted', {
-    input: z.object({ type: z.enum(['store', 'keep', 'release']) }),
-    resolve: async () => {
-      return [
-        {
-          id: '123',
-          dexId: '25',
-          spriteUrl:
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
-          jpName: 'Pika-shu',
-          enName: 'Pikachu',
-          vote: {
-            percentage: 78,
-            count: 42
-          }
+  .query('mostStored', {
+    resolve: async ({ ctx }) => {
+      const mostVoted = await ctx.prisma.$queryRaw<MostVotedResult[]>(mostStoredQuery);
+
+      const pokemons = await Promise.all(
+        mostVoted.map(({ pokemonId }) => {
+          return ctx.prisma.pokemon.findFirstOrThrow({
+            where: {
+              id: pokemonId
+            },
+            include: {
+              voteCount: {
+                select: {
+                  storeCount: true,
+                  keepCount: true,
+                  releaseCount: true
+                }
+              }
+            }
+          });
+        })
+      );
+
+      return pokemons.map((pokemon) => {
+        const { id, dex_id, sprite_url, jp_name, en_name, voteCount } = pokemon;
+
+        const result = {
+          id: id,
+          dexId: dex_id,
+          spriteUrl: sprite_url,
+          jpName: jp_name,
+          enName: en_name
+        };
+
+        if (!voteCount) {
+          return {
+            ...result,
+            vote: null
+          };
         }
-      ];
+
+        const { storeCount, keepCount, releaseCount } = voteCount;
+
+        const total = storeCount + keepCount + releaseCount;
+
+        return {
+          ...result,
+          vote: {
+            percentage: (storeCount / total) * 100,
+            count: storeCount
+          }
+        };
+      });
+    }
+  })
+  .query('mostKeeped', {
+    resolve: async ({ ctx }) => {
+      const mostVoted = await ctx.prisma.$queryRaw<MostVotedResult[]>(mostKeepedQuery);
+
+      const pokemons = await Promise.all(
+        mostVoted.map(({ pokemonId }) => {
+          return ctx.prisma.pokemon.findFirstOrThrow({
+            where: {
+              id: pokemonId
+            },
+            include: {
+              voteCount: {
+                select: {
+                  storeCount: true,
+                  keepCount: true,
+                  releaseCount: true
+                }
+              }
+            }
+          });
+        })
+      );
+
+      return pokemons.map((pokemon) => {
+        const { id, dex_id, sprite_url, jp_name, en_name, voteCount } = pokemon;
+
+        const result = {
+          id: id,
+          dexId: dex_id,
+          spriteUrl: sprite_url,
+          jpName: jp_name,
+          enName: en_name
+        };
+
+        if (!voteCount) {
+          return {
+            ...result,
+            vote: null
+          };
+        }
+
+        const { storeCount, keepCount, releaseCount } = voteCount;
+
+        const total = storeCount + keepCount + releaseCount;
+
+        return {
+          ...result,
+          vote: {
+            percentage: (keepCount / total) * 100,
+            count: keepCount
+          }
+        };
+      });
+    }
+  })
+  .query('mostReleased', {
+    resolve: async ({ ctx }) => {
+      const mostVoted = await ctx.prisma.$queryRaw<MostVotedResult[]>(mostReleasedQuery);
+
+      const pokemons = await Promise.all(
+        mostVoted.map(({ pokemonId }) => {
+          return ctx.prisma.pokemon.findFirstOrThrow({
+            where: {
+              id: pokemonId
+            },
+            include: {
+              voteCount: {
+                select: {
+                  storeCount: true,
+                  keepCount: true,
+                  releaseCount: true
+                }
+              }
+            }
+          });
+        })
+      );
+
+      return pokemons.map((pokemon) => {
+        const { id, dex_id, sprite_url, jp_name, en_name, voteCount } = pokemon;
+
+        const result = {
+          id: id,
+          dexId: dex_id,
+          spriteUrl: sprite_url,
+          jpName: jp_name,
+          enName: en_name
+        };
+
+        if (!voteCount) {
+          return {
+            ...result,
+            vote: null
+          };
+        }
+
+        const { storeCount, keepCount, releaseCount } = voteCount;
+
+        const total = storeCount + keepCount + releaseCount;
+
+        return {
+          ...result,
+          vote: {
+            percentage: (releaseCount / total) * 100,
+            count: releaseCount
+          }
+        };
+      });
     }
   });
+
+type MostVotedResult = {
+  pokemonId: string;
+  lowerBound: number;
+};
 
 const getVoteDetails = ({
   storeCount,
@@ -72,3 +224,23 @@ const getVoteDetails = ({
     release: { percentage: (releaseCount / total) * 100, count: releaseCount }
   };
 };
+
+const LIMIT = 10;
+
+const mostStoredQuery = Prisma.sql`SELECT "pokemonId", (("storeCount" + 1.9208) / ("storeCount" + ("releaseCount" + "keepCount")) -
+                        1.96 * SQRT(("storeCount" * ("releaseCount" + "keepCount")) / ("storeCount" + ("releaseCount" + "keepCount")) + 0.9604) /
+                        ("storeCount" + ("releaseCount" + "keepCount"))) / (1 + 3.8416 / ("storeCount" + ("releaseCount" + "keepCount")))
+                        AS "lowerBound" FROM "VoteCount" WHERE "storeCount" + ("releaseCount" + "keepCount") > 0
+                        ORDER BY "lowerBound" DESC LIMIT ${LIMIT};`;
+
+const mostKeepedQuery = Prisma.sql`SELECT "pokemonId", (("keepCount" + 1.9208) / ("keepCount" + ("releaseCount" + "storeCount")) -
+                        1.96 * SQRT(("keepCount" * ("releaseCount" + "storeCount")) / ("keepCount" + ("releaseCount" + "storeCount")) + 0.9604) /
+                        ("keepCount" + ("releaseCount" + "storeCount"))) / (1 + 3.8416 / ("keepCount" + ("releaseCount" + "storeCount")))
+                        AS "lowerBound" FROM "VoteCount" WHERE "keepCount" + ("releaseCount" + "storeCount") > 0
+                        ORDER BY "lowerBound" DESC LIMIT ${LIMIT};`;
+
+const mostReleasedQuery = Prisma.sql`SELECT "pokemonId", (("releaseCount" + 1.9208) / ("releaseCount" + ("storeCount" + "keepCount")) -
+                        1.96 * SQRT(("releaseCount" * ("storeCount" + "keepCount")) / ("releaseCount" + ("storeCount" + "keepCount")) + 0.9604) /
+                        ("releaseCount" + ("storeCount" + "keepCount"))) / (1 + 3.8416 / ("releaseCount" + ("storeCount" + "keepCount")))
+                        AS "lowerBound" FROM "VoteCount" WHERE "releaseCount" + ("storeCount" + "keepCount") > 0
+                        ORDER BY "lowerBound" DESC LIMIT ${LIMIT};`;
